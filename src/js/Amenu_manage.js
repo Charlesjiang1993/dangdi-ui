9abe8bf0f9755aac93c9ddac66515ffaff257440
jh909702901 <909702901@qq.com>
2017-05-16 17:46:26 +0800

    xx

--- src/js/Amenu_manage.js
+++ src/js/Amenu_manage.js
@@ -1,38 +1,18 @@
-window.onload=function(){
-    var oTab=document.getElementById('tab1');
-    var oBtn=document.getElementById('btn1');
-    var oName=document.getElementById('name');
-    
-    var createDate=function(){
-        var oTr=document.createElement('tr');
-        var oTd=document.createElement('td');
-        oTr.appendChild(oTd);
-        var oTd=document.createElement('td');
-        oTr.appendChild(oTd);
-        var oTd=document.createElement('td');
-        oTr.appendChild(oTd);
-        var oTd=document.createElement('td');
-        oTr.appendChild(oTd);
-        var oTd=document.createElement('td');
-        oTd.innerHTML='<a href="javascript:;">修改</a>/<a href="javascript:;">删除</a>';
+window.onload = function() {
+    var oTab = document.getElementById('tab1');
+
+    var createDate = function(n) {
+        var oTr = document.createElement('tr');
+        for (var i = 0; i < n - 1; i++) {
+            var oTd = document.createElement('td');
+            oTr.appendChild(oTd);
+        }
+        var oTd = document.createElement('td');
         oTr.appendChild(oTd);
         oTab.tBodies[0].appendChild(oTr);
-        
-    }   
-    
-    
-    oBtn.onclick=createDate();       
- 
-    var tabRemove=function(){
-        
+        console.log(oTr)
     }
-    var tabChange=function(){
-        
-    }
-    
-    
-    
-    
-    
-    
+
+
+
 }
